af302f55a54e6ee63ab9764649685eee25db9400
Tom O'Sullivan <tom@tomdotbat.dev>
2021-01-11 16:06:01 +0000

    Initial layout + config

---
+++ lua/autorun/sh_pixelkarts_loader.lua
@@ -0,0 +1,36 @@
+
+PIXEL = PIXEL or {}
+PIXEL.Karts = PIXEL.Karts or {}
+
+local function loadDirectory(dir)
+    local fil, fol = file.Find(dir .. "/*", "LUA")
+
+    for k,v in ipairs(fil) do
+        local dirs = dir .. "/" .. v
+
+        if v:StartWith("cl_") then
+            if SERVER then AddCSLuaFile(dirs)
+            else include(dirs) end
+        elseif v:StartWith("sh_") then
+            AddCSLuaFile(dirs)
+            include(dirs)
+        else
+            if SERVER then include(dirs) end
+        end
+    end
+
+    for k,v in pairs(fol) do
+        loadDirectory(dir .. "/" .. v)
+    end
+end
+
+local function loadAddon()
+    loadDirectory("pixelkarts")
+end
+
+if PIXEL.UI then
+    loadAddon()
+    return
+end
+
+hook.Add("PIXEL.UI.FullyLoaded", "PIXEL.Karts.WaitForPIXELUI", loadAddon)
---
+++ lua/pixelkarts/sh_config.lua
@@ -0,0 +1,75 @@
+print(type(true))
+PIXEL.Karts.Config = {
+    KartPrice = {
+        1500000, --Non VIP
+        1000000, --VIP
+        800000 --VIP Plus
+    },
+
+    Upgrades = {
+        CustomColor = {
+            Type = "Color",
+            Price = {
+                50000,
+                30000,
+                25000
+            }
+        },
+        RainbowMode = {
+            Type = "boolean",
+            Price = {
+                750000,
+                750000,
+                500000
+            },
+            RequiredLevel = 2
+        },
+        UnderGlow = {
+            Type = "Color",
+            Price = {
+                75000,
+                75000,
+                75000
+            },
+            RequiredLevel = 3
+        },
+        Nitro = {
+            Type = "boolean",
+            Price = {
+                200000,
+                150000,
+                125000
+            }
+        },
+        Glider = {
+            Type = "boolean",
+            Price = {
+                600000,
+                500000,
+                400000
+            },
+            RequiredLevel = 2
+        },
+        BuiltInRadio = {
+            Type = "boolean",
+            Price = {
+                20000,
+                20000,
+                15000
+            }
+        }
+    }
+}
+
+local meta = FindMetaTable("Player")
+
+function meta:PIXELKartsIsLevel(level)
+    if not level then return true end
+    if level == 0 or level == 1 then return true end
+
+    if level == 2 then
+        return self:IsUserGroup("vip") or self:IsUserGroup("vip+")
+    else
+        return self:IsUserGroup("vip+")
+    end
+end
