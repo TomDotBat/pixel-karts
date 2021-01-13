cd93a77e1beb8f1f29e19e7df02598959773ac06
Tom O'Sullivan <tom@tomdotbat.dev>
2021-01-13 10:58:58 +0000

    Kart enter/leave hook

--- lua/pixelkarts/core/cl_fake_ent_hooks.lua
+++ lua/pixelkarts/core/cl_fake_ent_hooks.lua
@@ -17,7 +17,21 @@ hook.Add("OnEntityCreated", "PIXEL.Karts.InitialiseKarts", function(ent)
     end)
 end)
 
+local localPly, curKart
 hook.Add("Think", "PIXEL.Karts.KartThink", function()
+    if not IsValid(localPly) then localPly = LocalPlayer() end
+
+    local kart = localPly:GetVehicle()
+    if IsValid(kart) and kart.IsPIXELKart then
+        if curKart ~= kart then
+            hook.Run("PIXEL.Karts.EnteredKart", kart)
+            curKart = kart
+        end
+    elseif curKart then
+        hook.Run("PIXEL.Karts.LeftKart", curKart)
+        curKart = nil
+    end
+
     for steamId, veh in pairs(karts) do
         if not IsValid(veh) then
             karts[steamId] = nil
