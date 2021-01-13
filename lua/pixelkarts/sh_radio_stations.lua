ae98246d3e161315e6762c698b9ca88601228899
Tom O'Sullivan <tom@tomdotbat.dev>
2021-01-13 21:42:01 +0000

    Radio station config

---
+++ lua/pixelkarts/sh_radio_stations.lua
@@ -0,0 +1,27 @@
+
+PIXEL.Karts.RadioStations = {
+	{
+		Name = "Drive Radio",
+		Link = "http://listen.radionomy.com/drive.m3u"
+	},
+	{
+		Name = "French Kiss FM",
+		Link = "http://stream.frenchkissfm.com/listen.pls"
+	},
+	{
+		Name = "Groove Salad",
+		Link = "http://somafm.com/groovesalad.pls"
+	},
+	{
+		Name = "Beat Blender",
+		Link = "http://somafm.com/beatblender.pls"
+	},
+	{
+		Name = "Plus FM",
+		Link = "http://www.plusfm.net/plusfm.pls"
+	},
+	{
+		Name = "GG Radio",
+		Link = "http://sv.ggradio.net:8000/stream"
+	}
+}
