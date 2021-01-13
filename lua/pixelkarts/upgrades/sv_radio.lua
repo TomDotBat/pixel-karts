f24c0fc850c4d29ded36bcf05378be2bb8f14e89
Tom O'Sullivan <tom@tomdotbat.dev>
2021-01-13 21:41:25 +0000

    Kart radio networking

---
+++ lua/pixelkarts/upgrades/sv_radio.lua
@@ -0,0 +1,28 @@
+
+util.AddNetworkString("PIXEL.Karts.Radio")
+
+net.Receive("PIXEL.Karts.Radio", function(len, ply)
+    local veh = ply:GetVehicle()
+    if not (IsValid(veh) and veh.IsPIXELKart) then return end
+
+    if veh.RadioLastChange and veh.RadioLastChange > CurTime() - 0.2 then return end
+    veh.RadioLastChange = CurTime()
+
+    local offset = net.ReadInt(8)
+    local curStation = veh:RadioGetChannel() or 0
+    if offset == 0 or not offset then
+        if curStation == 0 then
+            veh:RadioSetChannel(veh.RadioLastStation or 1)
+        else
+            veh.RadioLastStation = curStation
+            veh:RadioSetChannel(0)
+        end
+    else
+        local newStation = (curStation or 0) + offset
+        local stationCount = #PIXEL.Karts.RadioStations
+        if newStation > stationCount then newStation = 1 end
+        if newStation < 1 then newStation = stationCount end
+
+        veh:RadioSetChannel(newStation)
+    end
+end)
