590f31eb5a8c71a4a255c5878d111486530b09c0
Tom O'Sullivan <tom@tomdotbat.dev>
2021-01-13 16:26:37 +0000

    Glider ability HUD

--- lua/pixelkarts/upgrades/cl_glider.lua
+++ lua/pixelkarts/upgrades/cl_glider.lua
@@ -1,36 +1,33 @@
 
-local kart = LocalPlayer():GetVehicle()
-
 local keyName = PIXEL.Karts.Config.Upgrades.Glider.DeployKeyName
 local cooldown = PIXEL.Karts.Config.Upgrades.Glider.DeployCooldown
 
-hook.Add("HUDPaint", "PIXEL.Karts.GliderHUD", function()
-    if not IsValid(kart) then return end
-    --if not kart:GetNitro() then
-    --    hook.Remove("HUDPaint", "PIXEL.Karts.NitroBoostHUD")
-    --    return
-    --end
+PIXEL.RegisterScaledConstant("Karts.HUD.Spacing", 5)
 
-    local pad = PIXEL.Scale(3)
-    local dblPad = pad * 2
-    local progW, progH = PIXEL.Scale(300), PIXEL.Scale(30)
-    local progX, progY = ScrW() * .5 - progW * .5, ScrH() * .9
+hook.Add("PIXEL.Karts.EnteredKart", "PIXEL.Karts.GliderHUD", function(kart)
+    hook.Add("HUDPaint", "PIXEL.Karts.GliderHUD", function()
+        if not IsValid(kart) then return end
+        if not kart:GetGlider() then
+            hook.Remove("HUDPaint", "PIXEL.Karts.GliderHUD")
+            return
+        end
 
-    local cooldownProg = math.Clamp(1 - ((kart:GetNWFloat("PIXEL.Karts.NitroCooldown", 0) - CurTime()) / cooldown), 0, 1)
+        local pad = PIXEL.Scale(3)
+        local dblPad = pad * 2
+        local progW, progH = PIXEL.Scale(300), PIXEL.Scale(30)
+        local progX, progY = ScrW() * .5 - progW * .5, ScrH() * .9 + progH + PIXEL.GetScaledConstant("Karts.HUD.Spacing")
 
-    PIXEL.DrawRoundedBox(PIXEL.Scale(6), progX, progY, progW, progH, PIXEL.Colors.Background)
-    PIXEL.DrawRoundedBox(PIXEL.Scale(4), progX + pad, progY + pad, (progW - dblPad) * cooldownProg, progH - dblPad,
-        PIXEL.LerpColor(cooldownProg, PIXEL.Colors.Negative, PIXEL.Colors.Primary)
-    )
+        local cooldownProg = math.Clamp(1 - ((kart:GetNWFloat("PIXEL.Karts.GliderCooldown", 0) - CurTime()) / cooldown), 0, 1)
 
-    PIXEL.DrawSimpleText("Nitro Boost (" .. keyName .. ")", "PIXEL.Karts.NitroBoost", progX + progW * .5, progY + progH * .5, PIXEL.Colors.PrimaryText, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+        PIXEL.DrawRoundedBox(PIXEL.Scale(6), progX, progY, progW, progH, PIXEL.Colors.Background)
+        PIXEL.DrawRoundedBox(PIXEL.Scale(4), progX + pad, progY + pad, (progW - dblPad) * cooldownProg, progH - dblPad,
+            PIXEL.LerpColor(cooldownProg, PIXEL.Colors.Negative, PIXEL.Colors.Primary)
+        )
+
+        PIXEL.DrawSimpleText("Deploy Glider (" .. keyName .. ")", "PIXEL.Karts.RocketBoost", progX + progW * .5, progY + progH * .5, PIXEL.Colors.PrimaryText, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+    end)
 end)
 
-if true then return end
-
-hook.Add("PIXEL.Karts.EnteredKart", "PIXEL.Karts.NitroBoostHUD", function(kart)
-end)
-
-hook.Add("PIXEL.Karts.LeftKart", "PIXEL.Karts.NitroBoostHUD", function()
-    hook.Remove("HUDPaint", "PIXEL.Karts.NitroBoostHUD")
+hook.Add("PIXEL.Karts.LeftKart", "PIXEL.Karts.GliderHUD", function()
+    hook.Remove("HUDPaint", "PIXEL.Karts.GliderHUD")
 end)
