8c1a1e352a842efd220e6a9361e6798e729c530b
Tom O'Sullivan <tom@tomdotbat.dev>
2021-01-13 10:59:56 +0000

    Nitro networking, permissions and boosting

--- lua/pixelkarts/upgrades/sv_nitro.lua
+++ lua/pixelkarts/upgrades/sv_nitro.lua
@@ -1,18 +1,30 @@
 
 util.AddNetworkString("PIXEL.Karts.Nitro")
 
-net.Receive("PIXEL.Karts.Nitro", function(len, ply)
-    if not ply:InVehicle() then return end
+local boostKey = PIXEL.Karts.Config.Upgrades.Nitro.BoostKey
+hook.Add("KeyPress", "PIXEL.Karts.NitroBind", function(ply, key)
+    if key ~= boostKey then return end
 
     local veh = ply:GetVehicle()
-    if not veh.IsPIXELKart then return end
+    if not (IsValid(veh) and veh.IsPIXELKart) then return end
 
+    PIXEL.Karts.Nitro(ply, veh)
+end)
+
+function PIXEL.Karts.Nitro(ply, veh)
     if not veh:GetNitro() then
         PIXEL.Karts.Notify(ply, "Your kart doesn't have nitro loser.", 1)
         return
     end
 
-    if not ply:PIXELKartsIsLevel(PIXEL.Karts.Config.Nitro.RequiredLevel) then
+    if veh:GetNWFloat("PIXEL.Karts.NitroCooldown", 0) > CurTime() then
+        PIXEL.Karts.Notify(ply, "Your booster is still cooling down.", 1)
+        return
+    end
+
+    local nitroUpgrade = PIXEL.Karts.Config.Upgrades.Nitro
+
+    if not ply:PIXELKartsIsLevel(nitroUpgrade.RequiredLevel) then
         PIXEL.Karts.Notify(ply, "You don't have the required rank for nitro.", 1)
         return
     end
@@ -22,9 +34,11 @@ net.Receive("PIXEL.Karts.Nitro", function(len, ply)
         return
     end
 
+    veh:SetNWFloat("PIXEL.Karts.NitroCooldown", CurTime() + PIXEL.Karts.Config.Upgrades.Nitro.BoostCooldown)
+
     local phys = veh:GetPhysicsObject()
-    local boostPower = PIXEL.Karts.Config.Nitro.BoostPower
-    timer.Create("PIXEL.Karts.NitroBoost:" .. veh:EntIndex(), 0.025, 150, function()
+    local boostPower = nitroUpgrade.BoostPower
+    timer.Create("PIXEL.Karts.NitroBoost:" .. veh:EntIndex(), nitroUpgrade.BoostInterval, nitroUpgrade.BoostTime / nitroUpgrade.BoostInterval, function()
         if not IsValid(phys) then return end
         phys:ApplyForceCenter(veh:GetForward() * boostPower)
     end)
@@ -38,4 +52,4 @@ net.Receive("PIXEL.Karts.Nitro", function(len, ply)
     net.Start("PIXEL.Karts.Nitro")
     net.WriteEntity(veh)
     net.Send(recipients)
-end)
+end
