75731d972127e4a33af423cb00f780cc1ac3990c
Tom O'Sullivan <tom@tomdotbat.dev>
2021-01-15 21:09:03 +0000

    Update the database credentials

---
+++ lua/pixelkarts/sv_database.lua
@@ -0,0 +1,8 @@
+
+PIXEL.Karts.DatabaseCredentials = {
+    Host = "eu-1.pixelnetwork.co",
+    Post = 3306,
+    Database = "s9_pixel_karts",
+    Username = "u9_xStzGvhkfI",
+    Password = "X!WL5BRFtUCpK+jxwN5pwqUo"
+}
