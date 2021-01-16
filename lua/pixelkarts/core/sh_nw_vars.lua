2d94dd6ff91a1b25aa74def01a704dce6bdab537
Tom O'Sullivan <tom@tomdotbat.dev>
2021-01-16 15:22:09 +0000

    Add clientisde entity support for karts

--- lua/pixelkarts/core/sh_nw_vars.lua
+++ lua/pixelkarts/core/sh_nw_vars.lua
@@ -1,5 +1,27 @@
 
 function PIXEL.Karts.SetupNWVars(kart)
+    if kart.IsClientside then
+        kart.KartVars = {}
+        for upgradeName, upgrade in pairs(PIXEL.Karts.Config.Upgrades) do
+            if upgrade.Type == "boolean" then
+                kart.KartVars[upgradeName] = false
+            elseif upgrade.Type == "Color" then
+                kart.KartVars[upgradeName] = Color(255, 255, 255)
+            elseif upgrade.Type == "number" then
+                kart.KartVars[upgradeName] = -1
+            end
+
+            kart["Get" .. upgradeName] = function(s)
+                return kart.KartVars[upgradeName]
+            end
+
+            kart["Set" .. upgradeName] = function(s, new)
+                kart.KartVars[upgradeName] = new
+            end
+        end
+        return
+    end
+
     for upgradeName, upgrade in pairs(PIXEL.Karts.Config.Upgrades) do
         if upgrade.Type == "boolean" then
             kart["Get" .. upgradeName] = function(s)
