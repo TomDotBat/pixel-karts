dc410355716ee572925ef425b19ba1a54a56ebc5
Tom O'Sullivan <tom@tomdotbat.dev>
2021-01-16 11:17:36 +0000

    Network spam prevention

--- lua/pixelkarts/data/sv_player_data.lua
+++ lua/pixelkarts/data/sv_player_data.lua
@@ -1,99 +1,4 @@
 
-require("mysqloo")
-
-local tblPlyData = "pixel_karts_player_data"
-
-local database = PIXEL.Karts.Database
-if not database then
-    local credentials = PIXEL.Karts.DatabaseCredentials
-    PIXEL.Karts.Database = mysqloo.connect(
-        credentials.Host,
-        credentials.Username,
-        credentials.Password,
-        credentials.Database,
-        credentials.Port
-    )
-
-    database = PIXEL.Karts.Database
-    database:setMultiStatements(true)
-    database:setAutoReconnect(true)
-
-    hook.Add("InitPostEntity", "PIXEL.Karts.InitialiseDatabase", function()
-        database:connect()
-    end)
-end
-
-function database:onConnected()
-    local q = self:query(string.format([[CREATE TABLE IF NOT EXISTS %s (steamid VARCHAR(17) NOT NULL, data TEXT NOT NULL, CONSTRAINT %s_pk PRIMARY KEY (steamid));]], tblPlyData, tblPlyData))
-
-    function q:onError(err, sql)
-        print("[PIXEL Karts] WARNING - Database table creation failed.\n" .. err)
-    end
-
-    q:start()
-end
-
-function database:onConnectionFailed(err)
-    print("[PIXEL Karts] WARNING - Database connection failed.\n" .. err)
-end
-
-function PIXEL.Karts.GetPlayerData(steamid, callback)
-    local q = database:query(string.format([[SELECT data FROM %s WHERE steamid = "%s";]], tblPlyData, steamid))
-
-    function q:onSuccess(data)
-        local row = data[1]
-        if not row then callback(false) return end
-
-        local json = row["data"]
-        data = util.JSONToTable(json)
-        if not data then
-            callback(false, nil, json)
-            return
-        end
-
-        PIXEL.Karts.CachePlayerData(steamid, data, json)
-        callback(true, data, json)
-    end
-
-    function q:onError(err, sql)
-        print("[PIXEL Karts] WARNING - Database failed to get player data for '" .. steamid .. "'.\n" .. err)
-        callback(false)
-    end
-
-    q:start()
-end
-
-function PIXEL.Karts.SetPlayerData(steamid, data, callback)
-    data = util.TableToJSON(data)
-    local q = database:query(string.format([[INSERT INTO %s(steamid, data) VALUES ("%s", "%s") ON DUPLICATE KEY UPDATE steamid="%s", data="%s";]], tblPlyData, steamid, data, steamid, data))
-
-    function q:onSuccess()
-        PIXEL.Karts.ClearCachedPlayerData(steamid)
-        if callback then callback(true, data) end
-    end
-
-    function q:onError(err, sql)
-        print("[PIXEL Karts] WARNING - Database failed to get player data for '" .. steamid .. "'.\n" .. err)
-        if callback then callback(false, data) end
-    end
-
-    q:start()
-end
-
-function PIXEL.Karts.UpdatePlayerData(steamid, newData, callback)
-    PIXEL.Karts.GetPlayerData(steamid, function(success, data)
-        if success then
-            table.Merge(data, newData)
-        else
-            data = newData
-        end
-
-        PIXEL.Karts.SetPlayerData(steamid, data, function(success2, json)
-            if success2 then PIXEL.Karts.CachePlayerData(steamid, data, json) end
-            callback(success2, data, json)
-        end)
-    end)
-end
 
 PIXEL.Karts.PlayerDataCache = PIXEL.Karts.PlayerDataCache or {}
 PIXEL.Karts.PlayerDataCacheJson = PIXEL.Karts.PlayerDataCacheJson or {}
@@ -124,8 +29,12 @@ hook.Add("PlayerDisconnected", "PIXEL.Karts.ClearPlayerCache", function(ply)
     PIXEL.Karts.ClearCachedPlayerData(ply:SteamID64())
 end)
 
-function PIXEL.Karts.SendPlayerData(_, ply)
+function PIXEL.Karts.SendPlayerData(ply)
     local steamid = ply:SteamID64()
+
+    if timer.Exists("PIXEL.Karts.PlayerDataCooldown:" .. steamid) then return end
+    timer.Create("PIXEL.Karts.PlayerDataCooldown:" .. steamid, 30, 1, function() end)
+
     local json = PIXEL.Karts.PlayerDataCacheJson[steamid]
     if not json then
         PIXEL.Karts.GetPlayerData(steamid, function(success, data, newJson)
@@ -150,6 +59,9 @@ function PIXEL.Karts.SendPlayerData(_, ply)
      net.WriteData(json, len)
     net.Send(ply)
 end
-net.Receive("PIXEL.Karts.UpdatePlayerData", PIXEL.Karts.SendPlayerData)
+
+net.Receive("PIXEL.Karts.UpdatePlayerData", function(len, ply)
+    PIXEL.Karts.SendPlayerData(ply)
+end)
 
 util.AddNetworkString("PIXEL.Karts.UpdatePlayerData")
