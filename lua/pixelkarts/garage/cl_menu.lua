a076a0be8c4f60a5e21f8921a7a9ed4e91ef3284
Tom O'Sullivan <tom@tomdotbat.dev>
2021-01-19 20:18:35 +0000

    Menu open/close helpers

---
+++ lua/pixelkarts/garage/cl_menu.lua
@@ -0,0 +1,20 @@
+
+function PIXEL.Karts.OpenGarageMenu(data)
+    PIXEL.Karts.FlipGarageDoor(true)
+    PIXEL.Karts.StartGarageRadio()
+    PIXEL.Karts.OpenGarageDoor()
+    PIXEL.Karts.SpawnDecorations()
+    PIXEL.Karts.CreatePreviewKart()
+    PIXEL.Karts.SetupGarageCamera()
+end
+
+function PIXEL.Karts.CloseGarageMenu()
+    PIXEL.Karts.FadeOutGarageRadio()
+    PIXEL.Karts.CloseGarageDoor()
+    PIXEL.Karts.RemoveGarageCamera()
+    PIXEL.Karts.RemoveDecorations()
+
+    timer.Simple(1, function()
+        PIXEL.Karts.FlipGarageDoor(false)
+    end)
+end
