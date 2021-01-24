f5ddad1b043adcc13ccf14b8485ef65deafa4fb4
Tom O'Sullivan <tom@tomdotbat.dev>
2021-01-24 17:05:50 +0000

    Hide the HUD in the garage

--- lua/pixelkarts/garage/cl_menu.lua
+++ lua/pixelkarts/garage/cl_menu.lua
@@ -1,12 +1,26 @@
 
 function PIXEL.Karts.OpenGarageMenu(data)
+    local localPly = LocalPlayer()
+
+    if PIXEL.HUD and IsValid(PIXEL.HUD.Panel) then
+        PIXEL.HUD.Panel:SetVisible(false)
+    end
+
+    hook.Add("HUDShouldDraw", "PIXEL.Karts.HideHUD", function(name) return false end)
+
     PIXEL.Karts.FlipGarageDoor(true)
-    PIXEL.Karts.StartGarageRadio()
-    PIXEL.Karts.CreatePreviewKart()
     PIXEL.Karts.SetupGarageCamera()
+    PIXEL.Karts.StartGarageRadio()
+    PIXEL.Karts.CreatePreviewKart(localPly:InVehicle())
 end
 
 function PIXEL.Karts.CloseGarageMenu()
+    if PIXEL.HUD and IsValid(PIXEL.HUD.Panel) then
+        PIXEL.HUD.Panel:SetVisible(true)
+    end
+
+    hook.Remove("HUDShouldDraw", "PIXEL.Karts.HideHUD")
+
     PIXEL.Karts.FadeOutGarageRadio()
     PIXEL.Karts.CloseGarageDoor()
     PIXEL.Karts.RemoveGarageCamera()
