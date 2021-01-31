558b5d8ed93a0431264bf7a1f9507e6c2383dd3e
Tom O'Sullivan <tom@tomdotbat.dev>
2021-01-31 12:04:53 +0000

    Rename the menu file to the more appropriate "main"

--- lua/pixelkarts/garage/cl_main.lua
+++ lua/pixelkarts/garage/cl_main.lua
@@ -8,15 +8,33 @@ function PIXEL.Karts.OpenGarageMenu(data)
 
     hook.Add("HUDShouldDraw", "PIXEL.Karts.HideHUD", function(name) return false end)
 
+    local inVehicle = localPly:InVehicle()
+
     PIXEL.Karts.FlipGarageDoor(true)
     PIXEL.Karts.SetupGarageCamera()
     PIXEL.Karts.StartGarageRadio()
-    PIXEL.Karts.CreatePreviewKart(localPly:InVehicle())
+    PIXEL.Karts.CreatePreviewKart()
+
+    if data["owned"] and inVehicle then
+        if inVehicle then
+            --go straight to upgrader
+        else
+            if IsValid(PIXEL.Karts.GarageMenu) then PIXEL.Karts.GarageMenu:Remove() end
+            PIXEL.Karts.GarageMenu = vgui.Create("PIXEL.Karts.SpawnKartMenu")
+        end
+    else
+        if IsValid(PIXEL.Karts.GarageMenu) then PIXEL.Karts.GarageMenu:Remove() end
+        PIXEL.Karts.GarageMenu = vgui.Create("PIXEL.Karts.PurchaseKartMenu")
+    end
 end
 
 function PIXEL.Karts.CloseGarageMenu()
     if PIXEL.HUD and IsValid(PIXEL.HUD.Panel) then
         PIXEL.HUD.Panel:SetVisible(true)
+    end
+
+    if IsValid(PIXEL.Karts.GarageMenu) then
+        PIXEL.Karts.GarageMenu:Close()
     end
 
     hook.Remove("HUDShouldDraw", "PIXEL.Karts.HideHUD")
