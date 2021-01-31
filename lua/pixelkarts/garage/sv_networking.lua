30c39444e694d181e3395b9685370b8c2ba0e47a
Tom O'Sullivan <tom@tomdotbat.dev>
2021-01-31 15:42:13 +0000

    Fix a bunch of race conditions

--- lua/pixelkarts/garage/sv_networking.lua
+++ lua/pixelkarts/garage/sv_networking.lua
@@ -1,16 +1,25 @@
 
 local garageConfig = PIXEL.Karts.Config.Garage
 
-net.Receive("PIXEL.Karts.GarageStateUpdate", function(len, ply)
-    local steamid = ply:SteamID64()
-    if timer.Exists("PIXEL.Karts.GarageStateCooldown:" .. steamid) then return end
-    timer.Create("PIXEL.Karts.GarageStateCooldown:" .. steamid, 2, 1, function() end)
+local function togglePlayerGarageState(ply)
+    if ply:GetNWBool("PIXEL.Karts.IsInGarage", false) then
+        ply:SetNWBool("PIXEL.Karts.IsInGarage", false)
+        ply:SetNWBool("PIXEL.Karts.IsInGarageWithKart", false)
 
-    local inGarage = ply:GetNWBool("PIXEL.Karts.IsInGarage", false)
-    if not inGarage then
+        ply:SetPos(garageConfig.LeavePosition)
+        ply:SetEyeAngles(garageConfig.LeaveAngles)
+
+        net.Start("PIXEL.Karts.GarageStateUpdate")
+        net.Send(ply)
+    else
         if ply:Health() < 1 then
             ply:SetNWBool("PIXEL.Karts.IsInGarage", false)
             ply:SetNWBool("PIXEL.Karts.IsInGarageWithKart", false)
+            return
+        end
+
+        if not ply:GetPos():WithinAABox(garageConfig.EntryBoxPoint1, garageConfig.EntryBoxPoint2) then
+            PIXEL.Karts.Notify(ply, "Where the fuck are you?", 1, 5)
             return
         end
 
@@ -23,9 +32,14 @@ net.Receive("PIXEL.Karts.GarageStateUpdate", function(len, ply)
                 return
             end
 
+            veh:GetPhysicsObject():EnableMotion(false)
+            veh:SetColor(color_transparent)
+            veh:SetRenderMode(RENDERMODE_TRANSCOLOR)
+
+            ply:SetNWBool("PIXEL.Karts.IsInGarage", true)
             ply:SetNWBool("PIXEL.Karts.IsInGarageWithKart", true)
 
-            timer.Simple(.2, function()
+            timer.Simple(.6, function()
                 if IsValid(ply) then
                     ply:ExitVehicle()
                     ply:SetPos(garageConfig.InsidePositions[math.random(#garageConfig.InsidePositions)])
@@ -36,29 +50,57 @@ net.Receive("PIXEL.Karts.GarageStateUpdate", function(len, ply)
                 end
             end)
 
-            ply:SetNWBool("PIXEL.Karts.IsInGarage", not inGarage)
             return
         end
 
-        if not ply:GetPos():WithinAABox(garageConfig.EntryBoxPoint1, garageConfig.EntryBoxPoint2) then
-            PIXEL.Karts.Notify(ply, "Where the fuck are you?", 1, 5)
-            return
-        end
+        ply:SetNWBool("PIXEL.Karts.IsInGarage", true)
+        ply:SetNWBool("PIXEL.Karts.IsInGarageWithKart", true)
 
         ply:SetPos(garageConfig.InsidePositions[math.random(#garageConfig.InsidePositions)])
-    else
-        ply:SetNWBool("PIXEL.Karts.IsInGarageWithKart", false)
-        ply:SetPos(garageConfig.LeavePosition)
-        ply:SetEyeAngles(garageConfig.LeaveAngles)
+    end
+end
 
-        net.Start("PIXEL.Karts.GarageStateUpdate")
-        net.Send(ply)
+net.Receive("PIXEL.Karts.GarageStateUpdate", function(len, ply)
+    togglePlayerGarageState(ply)
+end)
+util.AddNetworkString("PIXEL.Karts.GarageStateUpdate")
+
+
+net.Receive("PIXEL.Karts.PurchaseKart", function(len, ply)
+    if not ply:GetNWBool("PIXEL.Karts.IsInGarage", false) then return end
+
+    local price = PIXEL.Karts.Config.KartPrice[ply:PIXELKartsGetLevel()]
+    if not ply:canAfford(price) then
+        togglePlayerGarageState(ply)
+        PIXEL.Karts.Notify(ply, "You can't afford to buy a kart.", 1)
+        return
     end
 
-    ply:SetNWBool("PIXEL.Karts.IsInGarage", not inGarage)
-end)
+    ply:addMoney(-price)
+    PIXEL.Karts.Notify(ply, "Purchased a kart for " .. DarkRP.formatMoney(price) .. ".", 1)
 
-util.AddNetworkString("PIXEL.Karts.GarageStateUpdate")
+    net.Start("PIXEL.Karts.RespawnKart")
+    net.Send(ply)
+end)
+util.AddNetworkString("PIXEL.Karts.PurchaseKart")
+
+net.Receive("PIXEL.Karts.RespawnKart", function(len, ply)
+    if not ply:GetNWBool("PIXEL.Karts.IsInGarage", false) then return end
+
+    local price = PIXEL.Karts.Config.RespawnPrice[ply:PIXELKartsGetLevel()]
+    if not ply:canAfford(price) then
+        togglePlayerGarageState(ply)
+        PIXEL.Karts.Notify(ply, "You can't afford to respawn your kart.", 1)
+        return
+    end
+
+    ply:addMoney(-price)
+    PIXEL.Karts.Notify(ply, "Respawned your kart for " .. DarkRP.formatMoney(price) .. ".", 1)
+
+    net.Start("PIXEL.Karts.RespawnKart")
+    net.Send(ply)
+end)
+util.AddNetworkString("PIXEL.Karts.RespawnKart")
 
 hook.Add("PlayerDeath", "PIXEL.Karts.LeaveGarageOnDeath", function(ply)
     if not ply:GetNWBool("PIXEL.Karts.IsInGarage", false) then return end
