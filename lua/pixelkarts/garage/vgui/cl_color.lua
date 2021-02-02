04078253d87e7fe7301b569c6ca001df3237b808
Tom O'Sullivan <tom@tomdotbat.dev>
2021-02-02 19:38:44 +0000

    Kart body colour picker

---
+++ lua/pixelkarts/garage/vgui/cl_color.lua
@@ -0,0 +1,100 @@
+
+local PANEL = {}
+
+PIXEL.RegisterFont("Karts.ColourLabels", "Open Sans SemiBold", 18)
+
+function PANEL:Init()
+    self.LeftContainer = vgui.Create("Panel", self)
+    self.LeftContainer:Dock(LEFT)
+
+    self.RightContainer = vgui.Create("Panel", self)
+    self.RightContainer:Dock(RIGHT)
+
+    self.ColorPicker = vgui.Create("PIXEL.ColorPicker", self.LeftContainer)
+    self.ColorPicker:Dock(FILL)
+
+    function self.ColorPicker.OnChange(s, color)
+        self.ColorEntry:SetValue("#" .. PIXEL.RGBToHex(color))
+        PIXEL.Karts.SprayPaintKart(PIXEL.Karts.PreviewKart, color)
+    end
+
+    function self.LeftContainer.PerformLayout(s, w, h)
+        local pickerPad = PIXEL.Scale(26)
+        self.ColorPicker:DockMargin(0, pickerPad, 0, pickerPad)
+    end
+
+    self.RightCenterContainer = vgui.Create("Panel", self.RightContainer)
+
+    function self.RightContainer.PerformLayout(s, w, h)
+        self.RightCenterContainer:SetWide(w)
+        self.RightCenterContainer:SizeToChildren(false, true)
+        self.RightCenterContainer:CenterVertical()
+    end
+
+    self.ColorEntryLabel = vgui.Create("PIXEL.Label", self.RightCenterContainer)
+    self.ColorEntryLabel:Dock(TOP)
+    self.ColorEntryLabel:SetText("Hex Color:")
+    self.ColorEntryLabel:SetFont("PIXEL.Karts.ColourLabels")
+    self.ColorEntryLabel:SetAutoHeight(true)
+
+    self.ColorEntry = vgui.Create("PIXEL.TextEntry", self.RightCenterContainer)
+    self.ColorEntry:Dock(TOP)
+    self.ColorEntry:SetValue("##000000")
+
+    self.RainbowModeContainer = vgui.Create("Panel", self.RightCenterContainer)
+    self.RainbowModeContainer:Dock(TOP)
+
+    self.RainbowCheckbox = vgui.Create("PIXEL.Checkbox", self.RainbowModeContainer)
+    self.RainbowCheckbox:Dock(LEFT)
+
+    local hsv, time = HSVToColor, CurTime
+    function self.RainbowCheckbox.Think(s)
+        if not s:GetToggle() then return end
+
+        self.ColorPicker:SetColor(hsv((time() * 20) % 360, 1, 1))
+        self.ColorPicker:UpdateColor()
+    end
+
+    self.RainbowLabel = vgui.Create("PIXEL.Label", self.RainbowModeContainer)
+    self.RainbowLabel:Dock(LEFT)
+    self.RainbowLabel:SetText("Rainbow Mode")
+    self.RainbowLabel:SetFont("PIXEL.Karts.ColourLabels")
+
+    function self.RainbowModeContainer.PerformLayout(s, w, h)
+        self.RainbowCheckbox:SetWide(h)
+        self.RainbowLabel:SetWide(PIXEL.Scale(140))
+        self.RainbowLabel:DockMargin(PIXEL.Scale(4), 0, 0, 0)
+    end
+
+    function self.RightCenterContainer.PerformLayout(s, w, h)
+        self.ColorEntry:DockMargin(0, PIXEL.Scale(2), PIXEL.Scale(6), 0)
+        self.ColorEntry:SetTall(PIXEL.Scale(32))
+
+        self.RainbowModeContainer:DockMargin(0, PIXEL.Scale(8), 0, 0)
+        self.RainbowModeContainer:SetTall(PIXEL.Scale(20))
+    end
+
+    function self.ColorEntry.OnEnter(s)
+        local value = s:GetValue()
+        if not string.find(value, "#[0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f]") then return end
+
+        local color = PIXEL.HexToRGB(value)
+        if not color then return end
+
+        self.ColorPicker:SetColor(color)
+    end
+end
+
+function PANEL:LayoutContent(w, h)
+    local desiredH = PIXEL.Scale(250)
+    if self:GetTall() ~= desiredH then
+        self:SetTall(desiredH)
+        self:SetWide(PIXEL.Scale(380))
+    end
+
+    local halfW = w * .47
+    self.LeftContainer:SetWide(halfW)
+    self.RightContainer:SetWide(halfW)
+end
+
+vgui.Register("PIXEL.Karts.ColorEditor", PANEL, "PIXEL.Karts.BaseUpgradeEditor")
