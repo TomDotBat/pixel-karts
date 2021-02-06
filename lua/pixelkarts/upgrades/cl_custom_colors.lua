50a2d165863d947fdbf0b6c473a7e7f2fb46f879
Tom O'Sullivan <tom@tomdotbat.dev>
2021-02-06 12:12:29 +0000

    Remove random unused getter function

--- lua/pixelkarts/upgrades/cl_custom_colors.lua
+++ lua/pixelkarts/upgrades/cl_custom_colors.lua
@@ -4,7 +4,5 @@ local hsv, time = HSVToColor, CurTime
 hook.Add("PIXEL.Karts.Think", "PIXEL.Karts.CustomColors", function(kart)
     if kart:GetRainbowMode() then
         kart:SetColor(hsv((time() * 20) % 360, 1, 1))
-    else
-        kart:GetCustomColor()
     end
 end)
