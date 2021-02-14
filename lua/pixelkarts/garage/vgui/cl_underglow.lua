bdaa4def38d7f970e431b816b7cc481b4ac660c6
Tom O'Sullivan <tom@tomdotbat.dev>
2021-02-14 12:49:51 +0000

    Fix underglow resetting

--- lua/pixelkarts/garage/vgui/cl_underglow.lua
+++ lua/pixelkarts/garage/vgui/cl_underglow.lua
@@ -19,7 +19,7 @@ function PANEL:Init()
         end
 
         if colChanged or (self:GetOriginalDataKey(enabledDataKey, false) ~= self:GetDataKey(enabledDataKey, false)) then
-            self:AddReceiptItem("Underglow", config.Price[LocalPlayer():PIXELKartsGetLevel()], dataKey)
+            self:AddReceiptItem("Underglow", config.Price[LocalPlayer():PIXELKartsGetLevel()], colorDataKey, enabledDataKey)
         else
             self:RemoveReceiptItem("Underglow")
         end
