fda355cb6df11cd072eb6331af3be3c6a56bf8cd
Tom O'Sullivan <tom@tomdotbat.dev>
2021-02-15 17:10:04 +0000

    Add an upgrade menu for buying passenger seats

---
+++ lua/pixelkarts/garage/vgui/cl_passenger_seat.lua
@@ -0,0 +1,71 @@
+
+local PANEL = {}
+
+function PANEL:Init()
+    PIXEL.Karts.MoveGarageCamera(340, Vector(-40, -40, 25))
+
+    local config = PIXEL.Karts.Config.Upgrades.PassengerSeat
+    local dataKey = config.DataKey
+
+    local function updateReceipt()
+        if self:GetOriginalDataKey(dataKey, false) ~= self:GetDataKey(dataKey, false) then
+            self:AddReceiptItem("Passenger Seat", config.Price[LocalPlayer():PIXELKartsGetLevel()], dataKey)
+        else
+            self:RemoveReceiptItem("Passenger Seat")
+        end
+    end
+
+    self.DescriptionLabel = vgui.Create("PIXEL.Label", self)
+    self.DescriptionLabel:Dock(TOP)
+    self.DescriptionLabel:SetAutoHeight(true)
+    self.DescriptionLabel:SetFont("PIXEL.Karts.UpgradeLabel")
+    self.DescriptionLabel:SetAutoWrap(true)
+    self.DescriptionLabel:SetText("Buying a passenger seat allows another person to tag along for the ride with safety.")
+
+    self.BottomContainer = vgui.Create("Panel", self)
+    self.BottomContainer:Dock(TOP)
+
+    self.EnableCheckbox = vgui.Create("PIXEL.Checkbox", self.BottomContainer)
+    self.EnableCheckbox:Dock(LEFT)
+
+    local previewKart = PIXEL.Karts.PreviewKart
+    function self.EnableCheckbox.OnToggled(s, enabled, disableSound)
+        if not IsValid(previewKart) then return end
+        previewKart:SetPassengerSeat(enabled)
+
+        self:SetDataKey(dataKey, enabled)
+        updateReceipt()
+
+        if disableSound then return end
+        PIXEL.Karts.PlayUpgradeSound(previewKart)
+    end
+
+    self.EnableLabel = vgui.Create("PIXEL.Label", self.BottomContainer)
+    self.EnableLabel:Dock(LEFT)
+    self.EnableLabel:SetText("Enable Passenger Seat")
+    self.EnableLabel:SetFont("PIXEL.Karts.ColourLabels")
+    self.EnableLabel:SetAutoWidth(true)
+
+    function self.BottomContainer.PerformLayout(s, w, h)
+        self.EnableCheckbox:SetWide(PIXEL.Scale(22))
+
+        self.EnableLabel:SetWide(PIXEL.Scale(140))
+        self.EnableLabel:DockMargin(PIXEL.Scale(4), PIXEL.Scale(1), 0, 0)
+    end
+
+    timer.Simple(0, function()
+        if self:GetDataKey(dataKey, false) then
+            self.EnableCheckbox:DoClick(true)
+        end
+    end)
+end
+
+function PANEL:LayoutContent(w, h)
+    self.BottomContainer:DockMargin(0, PIXEL.Scale(8), 0, 0)
+    self.BottomContainer:SetTall(PIXEL.Scale(22))
+
+    self:SetWide(PIXEL.Scale(320))
+    self:SizeToChildren(false, true)
+end
+
+vgui.Register("PIXEL.Karts.PassengerSeatEditor", PANEL, "PIXEL.Karts.BaseUpgradeEditor")
