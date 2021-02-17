9804756e318e7df229d711d0fd1873a8ea1c928f
Tom O'Sullivan <tom@tomdotbat.dev>
2021-02-17 12:59:19 +0000

    Use the logo nwvars

--- lua/pixelkarts/upgrades/cl_logos.lua
+++ lua/pixelkarts/upgrades/cl_logos.lua
@@ -4,10 +4,12 @@ local size = 1024
 local xPos = -(size * .5)
 local logoPos, logoAngle = Vector(0, 23, 26.35), Angle(0, 180, 24)
 hook.Add("PIXEL.Karts.DrawKartExtras", "PIXEL.Karts.DrawLogos", function(kart, localPly)
+    if not kart:GetLogoEnabled() then return end
+
     cam.Start3D2D(kart:LocalToWorld(logoPos), kart:LocalToWorldAngles(logoAngle), .009)
     local oldClipping = DisableClipping(true)
 
-    PIXEL.DrawImgur(xPos, 0, size, size, "STCrxYK", white)
+    PIXEL.DrawImgur(xPos, 0, size, size, kart:GetLogo(), white)
 
     DisableClipping(oldClipping)
     cam.End3D2D()
