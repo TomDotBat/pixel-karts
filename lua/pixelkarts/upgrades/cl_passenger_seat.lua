a0e3ace5d73b869cec134380e655dc40f54cfffb
Tom O'Sullivan <tom@tomdotbat.dev>
2021-02-19 20:26:47 +0000

    Find the passenger seat on the client if the kart has one

--- lua/pixelkarts/upgrades/cl_passenger_seat.lua
+++ lua/pixelkarts/upgrades/cl_passenger_seat.lua
@@ -1,20 +1,34 @@
 
 local seatConfig = PIXEL.Karts.Config.Upgrades.PassengerSeat
 
-hook.Add("PIXEL.Karts.Think", "PIXEL.Karts.PassengerSeatModel", function(kart)
-    if not kart.IsClientside then return end
+hook.Add("PIXEL.Karts.Think", "PIXEL.Karts.PassengerSeatThink", function(kart)
+    if kart.IsClientside then
+        if kart:GetPassengerSeat() then
+            if not IsValid(kart.PassengerSeat) then
+                kart.PassengerSeat = ClientsideModel("models/nova/jeep_seat.mdl")
+                kart.PassengerSeat:SetParent(kart)
+                kart.PassengerSeat:SetModelScale(.6)
+            end
 
-    if kart:GetPassengerSeat() then
-        if not IsValid(kart.PassengerSeat) then
-            kart.PassengerSeat = ClientsideModel("models/nova/jeep_seat.mdl")
-            kart.PassengerSeat:SetParent(kart)
-            kart.PassengerSeat:SetModelScale(.6)
+            kart.PassengerSeat:SetPos(kart:LocalToWorld(seatConfig.SeatPos))
+            kart.PassengerSeat:SetAngles(kart:LocalToWorldAngles(seatConfig.SeatAngle))
+        elseif IsValid(kart.PassengerSeat) then
+            kart.PassengerSeat:Remove()
         end
 
-        kart.PassengerSeat:SetPos(kart:LocalToWorld(seatConfig.SeatPos))
-        kart.PassengerSeat:SetAngles(kart:LocalToWorldAngles(seatConfig.SeatAngle))
-    elseif IsValid(kart.PassengerSeat) then
-        kart.PassengerSeat:Remove()
+        return
+    end
+
+    if not kart:GetPassengerSeat() then return end
+    if kart.PassengerSeat then return end
+
+    for _, child in pairs(kart:GetChildren()) do
+        if not IsValid(child) then continue end
+        if child:IsVehicle() then
+            child.IsPIXELKartsPassengerSeat = true
+            kart.PassengerSeat = child
+            return
+        end
     end
 end)
 
