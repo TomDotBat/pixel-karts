f272613aa7637336f088699f5cc5c84deac1689c
Tom O'Sullivan <tom@tomdotbat.dev>
2021-02-26 09:41:28 +0000

    Add a !removekart command

--- lua/sam/modules/pixel_karts.lua
+++ lua/sam/modules/pixel_karts.lua
@@ -21,3 +21,27 @@ sam.command.new("forceleavegarage")
     })
 end)
 :End()
+
+sam.command.new("removekart")
+:SetCategory("PIXEL Karts")
+:Help("Forcefully removes a players' PIXEL Kart.")
+:SetPermission("removekart", "admin")
+
+:AddArg("player")
+
+:OnExecute(function(caller, targets)
+    if not PIXEL.Karts then return end
+
+    for i = 1, #targets do
+        local target = targets[i]
+        local kart = target:GetNWEntity("PIXEL.Karts.PersonalKart", nil)
+        if IsValid(kart) then
+            SafeRemoveEntity(kart)
+        end
+    end
+
+    sam.player.send_message(nil, "{A} removed {T}'s PIXEL Kart.", {
+        A = caller, T = targets
+    })
+end)
+:End()
