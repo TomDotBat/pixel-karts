2183f6ceb20f2ce801bd322f19903bb125d2322d
Tom O'Sullivan <tom@tomdotbat.dev>
2021-04-18 14:58:30 +0100

    feat: add an app icon color to the speedometer

--- lua/pixelkarts/screens/cl_speedometer.lua
+++ lua/pixelkarts/screens/cl_speedometer.lua
@@ -41,4 +41,4 @@ PIXEL.Karts.SteeringWheelScreen("speedometer", "Speedometer", function(screen, k
     end
 end, true, false)
 
-PIXEL.Karts.SteeringWheelApp("speedometer", "Speedometer", "9uSMiMR", "speedometer")
+PIXEL.Karts.SteeringWheelApp("speedometer", "Speedometer", "9uSMiMR", PIXEL.OffsetColor(PIXEL.Colors.Negative, 30), "speedometer")
