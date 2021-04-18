80bf05fd80e530c02bab160c169fdd8bd16a2cad
Tom O'Sullivan <tom@tomdotbat.dev>
2021-04-18 14:58:42 +0100

    feat: passenger seat options menu

---
+++ lua/pixelkarts/screens/cl_passenger_seat.lua
@@ -0,0 +1,6 @@
+
+PIXEL.Karts.SteeringWheelScreen("passenger_seat", "Passenger Seat", function(screen, kart, offset, w, h, localPly, fullH)
+
+end, true, false)
+
+PIXEL.Karts.SteeringWheelApp("passenger_seat", "Passenger Seat", "jwJLYWP", Color(112, 74, 16), "passenger_seat")
