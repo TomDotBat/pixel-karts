f994e675e6697247daa4c99962384a5228f84116
Tom O'Sullivan <tom@tomdotbat.dev>
2021-04-19 13:33:49 +0100

    refactor: delete the old database config

--- lua/pixelkarts/sv_database.lua
+++
@@ -1,8 +0,0 @@
-
-PIXEL.Karts.DatabaseCredentials = {
-    Host = "eu-1.pixelnetwork.co",
-    Post = 3306,
-    Database = "s45_pixelkarts",
-    Username = "u45_NLBrXa6ZMn",
-    Password = "t9wOswACLXfiGs@@^b0KGn.T"
-}
