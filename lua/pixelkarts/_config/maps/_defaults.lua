d21408a5db30a38992932be1db81d3642dafc0e2
Tom O'Sullivan <tom@tomdotbat.dev>
2021-04-19 13:22:13 +0100

    feat: add a default map config

---
+++ lua/pixelkarts/_config/maps/_defaults.lua
@@ -0,0 +1,8 @@
+
+--[[
+    This configuration file is a fallback to prevent the system from erroring,
+    it is not recommended change the contents of this file, or use it as a base
+    for your own map configs.
+]]
+
+local CONFIG = PIXEL.Karts.Config
