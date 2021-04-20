d3ecc545bc742a7362e52b417b3557d30d63143a
Tom O'Sullivan <tom@tomdotbat.dev>
2021-04-20 10:49:01 +0100

    feat: add a map config for tits_v2

--- lua/pixelkarts/_config/maps/rp_downtown_tits_v2.lua
+++ lua/pixelkarts/_config/maps/rp_downtown_tits_v2.lua
@@ -1,5 +1,111 @@
 
 local CONFIG = PIXEL.Karts.Config
+local GARAGE_CONFIG = CONFIG.Garage
+
+--[[
+    PIXEL Karts garage ramp configuration.
+
+    Some maps have extremely high kerbs which our poor little
+    kart can't actually climb. This can become a massive problem
+    when we want to drive in to the PIXEL Karts garage. The solution
+    is to make an invisible ramp. You can disable this entirely if
+    preferred, in favour for your own PermaProps system.
+]]
+
+CONFIG.GarageRamp = {
+    Enabled = true, --Set to false to disable spawning the ramp.
+    Model = "models/hunter/blocks/cube05x3x025.mdl",
+    Position = Vector(-1127.312500, -817.656250, -206.187500),
+    Angles = Angle(24.307251, 179.994507, 0)
+}
+
+--[[
+    PIXEL Karts garage door configuration.
+
+    This is for creating our own fake door. As the client cannot
+    control map doors by default, we delete it, make our own
+    and animate it how we like.
+]]
+
+CONFIG.GarageDoor = {
+    Model = "models/hunter/plates/plate3x3.mdl",
+    Position = Vector(-1034.875000, -818.687500, -125.937500),
+    Angles = Angle(90, 0, 180),
+    OpenOffset = Vector(0, 0, 118), --The change difference in position when the door is fully opened.
+    MaterialTexture = "metal/metaldoor061a", --The texture that should be applied to our fake door.
+    MaterialSettings = "center .5 .5 scale .36 .36 rotate 0 translate .642 .95", --Overrides the texture transform with our own so we can stretch/scale the material.
+    MakeCollisions = true --Creates a prop for players to collide with when they walk into the garage.
+}
+
+
+--[[
+    PIXEL Karts garage door mat configuration.
+
+    This is the 3D2D door mat that appears on the ground in front of the
+    garage when nearby. You can change the text and positioning/sizing of
+    it, or disable it entirely depending on your preference.
+]]
+
+CONFIG.GarageDoorMat = {
+    Enabled = true, --Set to false to disable the door mat.
+    Text = "PIXEL Karts", --The text that should appear on the door mat.
+    Position = Vector(-1037, -754, -195.9),
+    Angles = Angle(0, -90, 0),
+    Width = 1280,
+    Height = 820
+}
+
+GARAGE_CONFIG.RadioOrigin = Vector(-814.031250, -882.968750, -151.687500) --The point in space where the garage radio can be heard from.
+
+--[[
+    PIXEL Karts spawn position configuration.
+
+    Where the karts are spawned when players leave the garage, and their
+    rotation.
+]]
+
+GARAGE_CONFIG.SpawnPos = Vector(-1175.125000, -815.625000, -160)
+GARAGE_CONFIG.SpawnAngle = Angle(0, 90, 0)
+
+--[[
+    PIXEL Karts garage entry, inside and leave position configuration.
+
+    This is for the box check that runs on the client to see if they
+    are standing on the garage door mat. The door range check on the
+    server, the inside position (where players are hidden) of the garage,
+    and the position and angles the player should be put in when they
+    leave the garage without a kart.
+]]
+
+GARAGE_CONFIG.EntryBoxPoint1 = Vector(-1034.979858, -752.671753, -55.503555)
+GARAGE_CONFIG.EntryBoxPoint2 = Vector(-1118.906616, -882.049927, -199.116608)
+
+GARAGE_CONFIG.EntryRangeCheckPoint = Vector(-1079.401855, -816.696777, -175)
+GARAGE_CONFIG.EntryRangeCheckRadius = 120
+
+GARAGE_CONFIG.LeavePosition = Vector(-1078.805908, -597.136536, -195)
+GARAGE_CONFIG.LeaveAngles = Angle(0, 0, 0)
+
+GARAGE_CONFIG.InsidePosition = Vector(-559.405640, -392.599762, -131.968750)
+
+--[[
+    PIXEL Karts garage customisation position configuration.
+
+    This configures the placement of the kart in the garage,
+    the camera position and radius of the circle the spray paint
+    can should follow around the kart when changing colour.
+]]
+
+GARAGE_CONFIG.KartPos = Vector(-881.531250, -790.531250, -191.343750)
+GARAGE_CONFIG.KartAngles = Angle(0, -90, 0)
+
+GARAGE_CONFIG.CameraCircleRadius = 115
+GARAGE_CONFIG.CameraOffset = Vector(0, 0, 55) --The offset applied to the camera (usually height), from the kart.
+GARAGE_CONFIG.CameraAimOffset = Vector(2, 0, 12) --The offset from the kart position we should aim the camera at.
+GARAGE_CONFIG.CameraRotation = 40 --The starting rotation of the camera, as if it was in a circle around the kart.
+
+GARAGE_CONFIG.SprayPaintCircleRadius = 50
+GARAGE_CONFIG.SprayPaintOffset = Vector(0, 0, 25) --The offset from the kart that the spray paint can should be at.
 
 --[[
     Placement configuration for props within the PIXEL Karts Garage.
