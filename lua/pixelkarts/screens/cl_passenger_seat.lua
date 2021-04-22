e5f86ecee664bfe7168ccb739a5e53df40b11f29
Tom O'Sullivan <tom@tomdotbat.dev>
2021-04-22 14:14:02 +0100

    feat: remove passenger seat app

--- lua/pixelkarts/screens/cl_passenger_seat.lua
+++
@@ -1,8 +0,0 @@
-
-local appName = gmodI18n.getAddon("pixelkarts"):getString("passengerSeat")
-
-PIXEL.Karts.SteeringWheelScreen("passenger_seat", appName, function(screen, kart, offset, w, h, localPly, fullH)
-
-end, true, false)
-
-PIXEL.Karts.SteeringWheelApp("passenger_seat", appName, "jwJLYWP", Color(112, 74, 16), "passenger_seat")
