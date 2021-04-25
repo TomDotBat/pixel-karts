fbf92aaeec9b84cb5659a45aec59938a993bf24c
nosharp <nosharp1@gmail.com>
2021-04-25 22:04:55 +0100

    feat: add sqlite config option

--- lua/pixelkarts/_config/sv_database.lua
+++ lua/pixelkarts/_config/sv_database.lua
@@ -1,5 +1,12 @@
 
 --[[
+    If you don't have MYSQLOO please set this to true and ignore second part of
+    this file.
+
+]]--
+PIXEL.Karts.Config.Sqlite = true
+
+--[[
     Database configuration for PIXEL Karts.
 
     Populate this configuration table with your MySQL/MariaDB credentials.
@@ -9,9 +16,9 @@
 ]]
 
 PIXEL.Karts.Config.DatabaseCredentials = {
-    Host = "127.0.0.1", --The address of your database server, use "127.0.0.1" instead of "localhost".
+    Host = "localhost", --The address of your database server, use "127.0.0.1" instead of "localhost".
     Post = 3306, --The port your database is running on, usually 3306.
-    Database = "pixel_karts", --The database you want PIXEL Karts to use. Make sure it exists before use.
-    Username = "root",
-    Password = "password123"
+    Database = "db", --The database you want PIXEL Karts to use. Make sure it exists before use.
+    Username = "user",
+    Password = "pass"
 }
