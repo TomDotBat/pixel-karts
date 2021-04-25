e4e4df1d9c2b5a85fa7b3152db421c05c11c5edd
Tom O'Sullivan <tom@tomdotbat.dev>
2021-04-25 19:29:01 +0100

    fix: remove abilities screen

--- lua/pixelkarts/screens/cl_abilities.lua
+++
@@ -1,6 +0,0 @@
-
-PIXEL.Karts.SteeringWheelScreen("abilities", "Abilities", function(screen, kart, offset, w, h, localPly, fullH)
-
-end, true, false)
-
-PIXEL.Karts.SteeringWheelApp("abilities", "Abilities", "uL4bDBn", PIXEL.OffsetColor(PIXEL.Colors.Gold, 30), "abilities")
