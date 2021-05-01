907a2fe62aed39de762d823cfac19a03972b1339
Tom O'Sullivan <tom@tomdotbat.dev>
2021-05-01 14:15:15 +0100

    feat: license info command

--- lua/autorun/sh_pixelkarts_loader.lua
+++ lua/autorun/sh_pixelkarts_loader.lua
@@ -1,4 +1,8 @@
 
+--PIXEL Karts [{{ script_id }}] v{{ script_version_name }} ({{ script_version_release_type }})
+--Licensed to: {{ user_id }}
+--{{ user_id sha256 key }}
+
 PIXEL = PIXEL or {}
 PIXEL.Karts = PIXEL.Karts or {
     Config = {}
@@ -15,12 +19,12 @@ local function loadDirectory(dir)
             else include(dirs) end
         elseif v:StartWith("sh_") then
             AddCSLuaFile(dirs)
-            include(dirs)
+            include(dirs) --{{ user_id sha256 key }}
         else
             if SERVER then include(dirs) end
         end
     end
-
+--{{ user_id }}
     for k,v in pairs(fol) do
         loadDirectory(dir .. "/" .. v)
     end
@@ -36,6 +40,25 @@ if PIXEL.UI then
 end
 
 hook.Add("PIXEL.UI.FullyLoaded", "PIXEL.Karts.WaitForPIXELUI", loadAddon)
+
+hook.Add("libgmodstore_init", "7629_libgmodstore", function()
+    libgmodstore:InitScript(7629, "🏎️PIXEL Karts - Garage, Customisations, Abilities & More!", {
+        version = "{{ script_version_name }}",
+        licensee = "{{ user_id }}" --{{ user_id | 25 }}
+    })
+end)
+
+local function printLicenseInfo()
+    print([[================================================
+PIXEL Karts [{{ script_id }}] v{{ script_version_name }} ({{ script_version_release_type }})
+Licensed to: {{ user_id }}
+{{ user_id sha256 key }}
+================================================]])
+end
+
+printLicenseInfo()
+concommand.Add("pixel_karts_print_license", printLicenseInfo)
+
 
 if CLIENT then return end
 resource.AddWorkshop("2395634176")
