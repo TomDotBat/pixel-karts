0aba4607a4558ac55568c922d27e8dc4e3f5ceb1
Tom O'Sullivan <tom@tomdotbat.dev>
2021-05-20 11:37:57 +0100

    feat: stormfox 1 support

--- lua/pixelkarts/core/sh_stormfox.lua
+++ lua/pixelkarts/core/sh_stormfox.lua
@@ -2,3 +2,9 @@
 hook.Add("stormfox2.postinit", "PIXEL.Karts.StormFox2Integration", function()
     StormFox2.Terrain.Get("snow"):AddTextureSwap("models/freeman/gokart_tyres", "models/freeman/gokart_tyres_snow_diffuse")
 end)
+
+hook.Add("StormFox.TexHandler.Default", "PIXEL.Karts.StormFox1Integration", function()
+    return {
+        snow = {["models/freeman/gokart_tyres"] = "models/freeman/gokart_tyres_snow_diffuse"}
+    }
+end)
