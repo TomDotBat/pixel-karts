208e32fa685522653ff672ed2c102a56a97e8c1d
Tom O'Sullivan <tom@tomdotbat.dev>
2021-06-18 10:43:44 +0100

    feat: get eyetrace creation id helper cmd

---
+++ lua/pixelkarts/garage/sv_config_helpers.lua
@@ -0,0 +1,6 @@
+
+concommand.Add("pixel_karts_print_trace_creation_id", function(ply)
+    local tr = ply:GetEyeTrace()
+    if not tr.Hit then return end
+    ply:PrintMessage(HUD_PRINTCONSOLE, tr.Entity:GetCreationID())
+end)
