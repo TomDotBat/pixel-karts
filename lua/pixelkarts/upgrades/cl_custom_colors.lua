bf1387dd2df623e40573ceb48944ddc3be32c0b8
Tom O'Sullivan <tom@tomdotbat.dev>
2022-03-08 22:05:11 +0000

    chore: apply the GNU GPL v3 license

--- lua/pixelkarts/upgrades/cl_custom_colors.lua
+++ lua/pixelkarts/upgrades/cl_custom_colors.lua
@@ -1,5 +1,21 @@
-local hsv, time = HSVToColor, CurTime
+--[[
+    PIXEL Karts
+    Copyright (C) 2022 Thomas (Tom.bat) O'Sullivan 
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+]]
+
+local hsv, time = HSVToColor, CurTime
 
 hook.Add("PIXEL.Karts.Think", "PIXEL.Karts.CustomColors", function(kart)
     if kart:GetRainbowMode() then
