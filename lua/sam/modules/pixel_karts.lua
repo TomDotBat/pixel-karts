e1bb0dd6e86c90f66a0caab9a4b59db0ac2b63c8
Tom O'Sullivan <tom@tomdotbat.dev>
2022-03-09 09:51:01 +0000

    style: indent SAM commands

--- lua/sam/modules/pixel_karts.lua
+++ lua/sam/modules/pixel_karts.lua
@@ -16,83 +16,83 @@
 ]]
 
 sam.command.new("forceleavegarage")
-:SetCategory("PIXEL Karts")
-:Help("Forces a player to leave the PIXEL Karts garage.")
-:SetPermission("forceleavegarage", "admin")
+    :SetCategory("PIXEL Karts")
+    :Help("Forces a player to leave the PIXEL Karts garage.")
+    :SetPermission("forceleavegarage", "admin")
 
-:AddArg("player")
+    :AddArg("player")
 
-:OnExecute(function(caller, targets)
-    if not (PIXEL.Karts and PIXEL.Karts.TogglePlayerGarageState) then return end
+    :OnExecute(function(caller, targets)
+        if not (PIXEL.Karts and PIXEL.Karts.TogglePlayerGarageState) then return end
 
-    for i = 1, #targets do
-        local target = targets[i]
-        if target:GetNWBool("PIXEL.Karts.IsInGarage", false) then
-            PIXEL.Karts.TogglePlayerGarageState(target, true)
+        for i = 1, #targets do
+            local target = targets[i]
+            if target:GetNWBool("PIXEL.Karts.IsInGarage", false) then
+               PIXEL.Karts.TogglePlayerGarageState(target, true)
+            end
         end
-    end
 
-    sam.player.send_message(nil, "{A} kicked {T} out of the PIXEL Karts garage.", {
-        A = caller, T = targets
-    })
-end)
+        sam.player.send_message(nil, "{A} kicked {T} out of the PIXEL Karts garage.", {
+            A = caller, T = targets
+        })
+    end)
 :End()
 
 sam.command.new("removekart")
-:SetCategory("PIXEL Karts")
-:Help("Forcefully removes a players' PIXEL Kart.")
-:SetPermission("removekart", "admin")
+    :SetCategory("PIXEL Karts")
+    :Help("Forcefully removes a players' PIXEL Kart.")
+    :SetPermission("removekart", "admin")
 
-:AddArg("player")
+    :AddArg("player")
 
-:OnExecute(function(caller, targets)
-    if not PIXEL.Karts then return end
+    :OnExecute(function(caller, targets)
+        if not PIXEL.Karts then return end
 
-    for i = 1, #targets do
-        local target = targets[i]
-        local kart = target:GetNWEntity("PIXEL.Karts.PersonalKart", nil)
-        if IsValid(kart) then
-            SafeRemoveEntity(kart)
+        for i = 1, #targets do
+            local target = targets[i]
+            local kart = target:GetNWEntity("PIXEL.Karts.PersonalKart", nil)
+            if IsValid(kart) then
+                SafeRemoveEntity(kart)
+            end
         end
-    end
 
-    sam.player.send_message(nil, "{A} removed {T}'s PIXEL Kart.", {
-        A = caller, T = targets
-    })
-end)
+        sam.player.send_message(nil, "{A} removed {T}'s PIXEL Kart.", {
+            A = caller, T = targets
+        })
+    end)
 :End()
 
 sam.command.new("resetkartlogo")
-:SetCategory("PIXEL Karts")
-:Help("Forcefully sets a players' PIXEL Kart logo to the default.")
-:SetPermission("resetkartlogo", "admin")
+    :SetCategory("PIXEL Karts")
+    :Help("Forcefully sets a players' PIXEL Kart logo to the default.")
+    :SetPermission("resetkartlogo", "admin")
 
-:AddArg("player")
+    :AddArg("player")
 
-:OnExecute(function(caller, targets)
-    if not PIXEL.Karts then return end
+    :OnExecute(function(caller, targets)
+        if not PIXEL.Karts then return end
 
-    for i = 1, #targets do
-        local target = targets[i]
-        local logoConfig = PIXEL.Karts.Config.Upgrades.Logo
+        for i = 1, #targets do
+            local target = targets[i]
+            local logoConfig = PIXEL.Karts.Config.Upgrades.Logo
 
-        target:PIXELKartsGetDataKey(logoConfig.DataKey, function(imgurId)
-            if not imgurId then return end
-            if not IsValid(target) then return end
+            target:PIXELKartsGetDataKey(logoConfig.DataKey, function(imgurId)
+                if not imgurId then return end
+                if not IsValid(target) then return end
 
-            target:PIXELKartsSetDataKey(logoConfig.DataKey, logoConfig.DefaultLogoId)
-            BroadcastLua([[file.Delete("pixel/]] .. imgurId .. [[.png")]])
-        end)
+                target:PIXELKartsSetDataKey(logoConfig.DataKey, logoConfig.DefaultLogoId)
+                BroadcastLua([[file.Delete("pixel/]] .. imgurId .. [[.png")]])
+            end)
 
 
-        local kart = target:GetNWEntity("PIXEL.Karts.PersonalKart", nil)
-        if IsValid(kart) and kart.SetLogo then
-            kart:SetLogo(logoConfig.DefaultLogoId)
+            local kart = target:GetNWEntity("PIXEL.Karts.PersonalKart", nil)
+            if IsValid(kart) and kart.SetLogo then
+                kart:SetLogo(logoConfig.DefaultLogoId)
+            end
         end
-    end
 
-    sam.player.send_message(nil, "{A} reset {T}'s PIXEL Kart logo.", {
-        A = caller, T = targets
-    })
-end)
+        sam.player.send_message(nil, "{A} reset {T}'s PIXEL Kart logo.", {
+            A = caller, T = targets
+        })
+    end)
 :End()
